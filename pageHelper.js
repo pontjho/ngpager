2cde76d61bb3f710d9d976af0661df4d09393ded
Pontjho Mokhonoana <pontjho@entelect.co.za>
2014-08-04 22:10:08 +0200

    Initial commit

---
+++ pageHelper.js
@@ -0,0 +1,29 @@
+﻿(function(){
+'use strict';
+
+app.factory('PageHelper', [function () {
+    return {
+        init: function () {
+            return {
+                totalPages: 1,
+                pageNumber: 1,
+                pages: [],
+                hasPreviousPage: false,
+                hasNextPage: false
+            };
+        },
+        updatePageInfo: function (pageInfo, data) {
+            pageInfo.totalPages = data.totalPages;
+            pageInfo.pageNumber = data.pageNumber;
+            pageInfo.hasPreviousPage = data.hasPreviousPage;
+            pageInfo.hasNextPage = data.hasNextPage;
+            pageInfo.pages = [];
+
+            for (var i = 0; i < data.totalPages; i++) {
+                pageInfo.pages.push({ number: i, active: data.pageNumber == i + 1 });
+            }
+        }
+    };
+}]);
+
+})();
---
+++ pager.js
@@ -0,0 +1,60 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('Pager', [])
+    .directive('pager', [function () {
+        var definition = {
+            restrict: 'A',
+            scope: { currentPage: '=', totalPages: '=', maxPagesToDisplay: '@', pageChanged: '&' },
+            replace: true,
+            templateUrl: 'pager.tpl.html',
+
+            link: function (scope, iElement, iAttrs) {
+                scope.changed = function (newPage) {
+                    scope.pageChanged({ pageNum: newPage });
+                };
+
+                scope.$watch('currentPage', updateCurrentPage);
+                scope.$watch('totalPages', updateTotalPages);
+                scope.$watch('maxPagesToDisplay', updateMaxPages);
+
+                function updateTotalPages(totalPages)
+                {
+                    updatePages(scope.currentPage, totalPages, scope.maxPagesToDisplay);
+                }
+
+                function updateCurrentPage(currentPage)
+                {
+                    updatePages(currentPage, scope.totalPages, scope.maxPagesToDisplay);
+                }
+
+                function updateMaxPages(maxPages)
+                {
+                    updatePages(scope.currentPage, scope.totalPages, maxPages);
+                }
+
+                function updatePages(currentPage, totalPages, maxPages)
+                {
+                    var pages = [];
+                    currentPage = parseInt(currentPage);
+                    var pagesLeft = totalPages - currentPage;
+                    var pagesAfterCurrent = Math.min(totalPages - currentPage, maxPages);
+                    var pagesBeforeCurrent = Math.max(0, maxPages - pagesAfterCurrent);
+
+                    var start = currentPage - pagesBeforeCurrent;
+                    var end = currentPage + pagesAfterCurrent;
+
+                    scope.hasPreBuffer = start !== 0;
+                    scope.hasPostBuffer = end !== totalPages;
+
+                    for(var i = start; i < end; i++)
+                    {
+                        pages.push({pageNumber: i, isCurrent: i == currentPage });
+                    }
+                    scope.pages = pages;
+                }
+            }
+        };
+        return definition;
+    }]);
+})();
